856920933b95341d31396b08e146f588183b8200
stykle <Maxamadabdulahist@outlook.com>
2026-03-05 13:50:56 +0100

    commented IT test

--- src/test/java/com/rufus100procent/monitor/MonitorApplicationTests.java
+++ src/test/java/com/rufus100procent/monitor/MonitorApplicationTests.java
@@ -3,7 +3,7 @@ package com.rufus100procent.monitor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MonitorApplicationTests {
 
     @Test
