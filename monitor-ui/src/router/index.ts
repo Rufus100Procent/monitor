fa7b6234920973af4f94b8b296db106f360f3bf7
stykle <Maxamadabdulahist@outlook.com>
2026-03-07 08:44:09 +0100

    added routes

--- monitor-ui/src/router/index.ts
+++ monitor-ui/src/router/index.ts
@@ -2,7 +2,24 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', redirect: '/dashboard' },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: () => import('../views/DashboardView.vue'),
+    },
+    {
+      path: '/faq',
+      name: 'FAQ',
+      component: () => import('../views/FaqView.vue'),
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: () => import('../views/ProfileView.vue'),
+    },
+  ],
 })
 
 export default router
