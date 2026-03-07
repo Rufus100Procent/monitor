38e267b84d2fcab5e81d1bd1e4711c2b72d2a46b
stykle <Maxamadabdulahist@outlook.com>
2026-03-07 14:43:04 +0100

    added additional routes

--- monitor-ui/src/router/index.ts
+++ monitor-ui/src/router/index.ts
@@ -7,17 +7,40 @@ const router = createRouter({
     {
       path: '/dashboard',
       name: 'Dashboard',
+      meta: { title: 'Dashboard' },
       component: () => import('../views/DashboardView.vue'),
     },
     {
       path: '/faq',
       name: 'FAQ',
+      meta: { title: 'FAQ' },
       component: () => import('../views/FaqView.vue'),
     },
     {
       path: '/profile',
       name: 'Profile',
+      meta: { title: 'Profile' },
       component: () => import('../views/ProfileView.vue'),
+    },
+    {
+      path: '/server/:id',
+      children: [
+        {
+          path: 'basics',
+          meta: { title: 'Basics' },
+          component: () => import('../views/server/BasicsView.vue'),
+        },
+        {
+          path: 'track-me',
+          meta: { title: 'Track Me' },
+          component: () => import('../views/server/TrackMeView.vue'),
+        },
+        {
+          path: 'settings',
+          meta: { title: 'Settings' },
+          component: () => import('../views/server/SettingsView.vue'),
+        },
+      ],
     },
   ],
 })
